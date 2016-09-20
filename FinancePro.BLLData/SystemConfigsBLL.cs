724cab93b267d518faeac183e65b0dc3521dce7f
AnEmptyCup <chenghang1937@hotmail.com>
2016-09-20 15:26:49 +0800

    提交代码

---
+++ FinancePro.BLLData/SystemConfigsBLL.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinancePro.DALData;
+using FinancePro.DataModels;
+
+namespace FinancePro.BLLData
+{
+    public class SystemConfigsBLL
+    {
+        /// <summary>
+        /// 根据ID查询配置项值
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public static string GetConfigsValueByID(int id)
+        {
+            return SystemConfigsDAL.GetConfigsValueByID(id);
+        }
+        /// <summary>
+        /// 添加新的配置信息
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public int AddNewSystemConfigs(SystemConfigsModel model)
+        {
+            return SystemConfigsDAL.AddNewSystemConfigs(model);
+        }
+        /// <summary>
+        /// 修改配置信息
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public int UpdateConfigs(SystemConfigsModel model)
+        {
+            return SystemConfigsDAL.UpdateConfigs(model);
+        }
+        /// <summary>
+        /// 查询所有的配置项
+        /// </summary>
+        /// <param name="isadmin"></param>
+        /// <returns></returns>
+        public List<SystemConfigsModel> GetAllConfigs(int isadmin)
+        {
+            return SystemConfigsDAL.GetAllConfigs(isadmin);
+        }
+    }
+}
---
+++ FinancePro/Controllers/AppContent.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FinancePro.BLLData;
+
+namespace FinancePro.Controllers
+{
+    public class AppContent
+    {
+        /// <summary>
+        /// 存储用户信息的session名称
+        /// </summary>
+        public static readonly string SESSION_LOGIN_NAME = "SESSION_LOGIN_NAME";
+        public static readonly string VALICODE = "VALICODE";
+        public static readonly string SESSION_WEB_LOGIN = "SESSION_WEB_LOGIN";
+        /// <summary>
+        /// 图片上传路径
+        /// </summary>
+        public static string UploadPath = System.Configuration.ConfigurationManager.AppSettings["uploadpath"];
+        /// <summary>
+        /// 存放图片的域名称
+        /// </summary>
+        public static string Imgdomain = System.Configuration.ConfigurationManager.AppSettings["imgdomain"];
+        /// <summary>
+        /// 允许上传文件类型
+        /// </summary>
+        public static string ImgType = System.Configuration.ConfigurationManager.AppSettings["imgtype"];
+        /// <summary>
+        /// 默认空图
+        /// </summary>
+        public static string DefaultEmptyImg = System.Configuration.ConfigurationManager.AppSettings["defaultemptyimg"];
+        /// <summary>
+        /// 网站加密字符串
+        /// </summary>
+        public static string SecrectStr = SystemConfigsBLL.GetConfigsValueByID(10);
+        /// <summary>
+        /// 网站使用模板名称
+        /// </summary>
+        public static string TempleteName = string.IsNullOrWhiteSpace(SystemConfigsBLL.GetConfigsValueByID(23)) ? "WebFrontArea" : SystemConfigsBLL.GetConfigsValueByID(23);
+    }
+}
