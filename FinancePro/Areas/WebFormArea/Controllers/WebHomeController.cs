55a63391eb9a5fcd19c773b9bc4d566509c4be3e
AnEmptyCup <chenghang1937@hotmail.com>
2016-10-25 19:02:53 +0800

    commit

--- FinancePro/Areas/WebFormArea/Controllers/WebHomeController.cs
+++ FinancePro/Areas/WebFormArea/Controllers/WebHomeController.cs
@@ -35,62 +35,125 @@ namespace FinancePro.Areas.WebFormArea.Controllers
         //添加会员
         public ActionResult AddMember()
         {
+            MemberInfoModel logmember = Session[AppContent.SESSION_WEB_LOGIN] as MemberInfoModel;
+            if (logmember == null)
+            {
+                return RedirectToAction("Index", "Login", new { area = "WebFormArea" });
+            }
             return View();
         }
-       //会员列表
+        //会员列表
         public ActionResult MemberList()
         {
+            MemberInfoModel logmember = Session[AppContent.SESSION_WEB_LOGIN] as MemberInfoModel;
+            if (logmember == null)
+            {
+                return RedirectToAction("Index", "Login", new { area = "WebFormArea" });
+            }
             return View();
         }
         //提现
         public ActionResult ApplyCash()
         {
+            MemberInfoModel logmember = Session[AppContent.SESSION_WEB_LOGIN] as MemberInfoModel;
+            if (logmember == null)
+            {
+                return RedirectToAction("Index", "Login", new { area = "WebFormArea" });
+            }
             return View();
         }
         //提现列表
         public ActionResult CashList()
         {
+            MemberInfoModel logmember = Session[AppContent.SESSION_WEB_LOGIN] as MemberInfoModel;
+            if (logmember == null)
+            {
+                return RedirectToAction("Index", "Login", new { area = "WebFormArea" });
+            }
             return View();
         }
         //会员转账
         public ActionResult MemberTransfer()
         {
+            MemberInfoModel logmember = Session[AppContent.SESSION_WEB_LOGIN] as MemberInfoModel;
+            if (logmember == null)
+            {
+                return RedirectToAction("Index", "Login", new { area = "WebFormArea" });
+            }
             return View();
         }
         //转账列表
         public ActionResult Transferlist()
         {
+            MemberInfoModel logmember = Session[AppContent.SESSION_WEB_LOGIN] as MemberInfoModel;
+            if (logmember == null)
+            {
+                return RedirectToAction("Index", "Login", new { area = "WebFormArea" });
+            }
             return View();
         }
         //收益明细日志
         public ActionResult CapitalDetailLog()
         {
+            MemberInfoModel logmember = Session[AppContent.SESSION_WEB_LOGIN] as MemberInfoModel;
+            if (logmember == null)
+            {
+                return RedirectToAction("Index", "Login", new { area = "WebFormArea" });
+            }
+
             return View();
         }
 
         //会员图谱
         public ActionResult MemberMap()
         {
+            MemberInfoModel logmember = Session[AppContent.SESSION_WEB_LOGIN] as MemberInfoModel;
+            if (logmember == null)
+            {
+                return RedirectToAction("Index", "Login", new { area = "WebFormArea" });
+            }
             return View();
         }
         //写消息
         public ActionResult WriteMessage()
         {
+            MemberInfoModel logmember = Session[AppContent.SESSION_WEB_LOGIN] as MemberInfoModel;
+            if (logmember == null)
+            {
+                return RedirectToAction("Index", "Login", new { area = "WebFormArea" });
+            }
             return View();
         }
         //消息列表
         public ActionResult MessageList()
         {
-            return View();
+            MemberInfoModel logmember = Session[AppContent.SESSION_WEB_LOGIN] as MemberInfoModel;
+            if (logmember == null)
+            {
+                return RedirectToAction("Index", "Login", new { area = "WebFormArea" });
+            }
+            MessageListViewModel model = new MessageListViewModel();
+            model.WebContactList = sitenewsbll.GetContractMessage(logmember.ID);
+            return View(model);
         }
         //用户资料
         public ActionResult UserInfo()
         {
+            MemberInfoModel logmember = Session[AppContent.SESSION_WEB_LOGIN] as MemberInfoModel;
+            if (logmember == null)
+            {
+                return RedirectToAction("Index", "Login", new { area = "WebFormArea" });
+            }
             return View();
         }
         //修改密码
         public ActionResult UpdatePwd()
         {
+            MemberInfoModel logmember = Session[AppContent.SESSION_WEB_LOGIN] as MemberInfoModel;
+            if (logmember == null)
+            {
+                return RedirectToAction("Index", "Login", new { area = "WebFormArea" });
+            }
             return View();
         }
     }
