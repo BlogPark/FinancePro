4a7ccf4afb7d1745418a0ebfc00034b61280c22b
zhangchenghang <chenghang1937@126.com>
2016-11-07 21:46:09 +0800

    commit

---
+++ FinancePro/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FinancePro.Filters;
+
+namespace FinancePro.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        [WebLoginAttribute]
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index", "Login", new { area = "WebFormArea" });
+        }
+
+    }
+}
